ee68bb2f387ab7f6c94b799a37b67d8a22a7248b
NishanthDevops8 <nishanthdevops8@gmail.com>
2023-10-04 12:48:34 +0530

    updated the command in sh

--- 01-sample.sh
+++ 01-sample.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 #shebang notation is to specify the interpretor type of script either bash or python or others.
 echo " welcome to shell scripting"
+# to run the sh 
+#commands are sh filename.sh or bash filename.sh or ./filename.sh
