8c3647a405964aae914552a9ff948c2caafbdb36
NishanthDevops8 <nishanthdevops8@gmail.com>
2023-10-05 16:01:43 +0530

    auto push

--- 03-colours.sh
+++ 03-colours.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #This is to define the colours 
 
 echo  -e "\e[34m this is the blue colour \e[0m"
---
+++ 04-Variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#This file is to discuss the variables.
+
+#What is variable ? Generally used to hold some value dynamically.
+# Variables are declared in the way
+a = 10
+b=20
+c= hell0
+echo "Print $a"
+echo "Print $b"
+echo "Print $c"
