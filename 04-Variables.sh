d9bb0687617854e97f59834c940fd7d2915cfc64
NishanthDevops8 <nishanthdevops8@gmail.com>
2023-10-05 16:02:48 +0530

    auto push

--- 04-Variables.sh
+++ 04-Variables.sh
@@ -4,9 +4,9 @@
 
 #What is variable ? Generally used to hold some value dynamically.
 # Variables are declared in the way
-a = 10
+a=10
 b=20
-c= hell0
+c=hell0
 echo "Print $a"
 echo "Print $b"
 echo "Print $c"
