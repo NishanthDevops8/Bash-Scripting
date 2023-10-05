fcc10b1624bb2e6a9ed3accebbd3f4c578dc771c
NishanthDevops8 <nishanthdevops8@gmail.com>
2023-10-05 16:06:40 +0530

    auto push

--- 04-Variables.sh
+++ 04-Variables.sh
@@ -3,6 +3,12 @@
 #This file is to discuss the variables.
 
 #What is variable ? Generally used to hold some value dynamically.
+# There is no data type in the shell scripting by default everything is string.
+# If your input or variable have some special characters it should be enclosed in the double quotation.
+#Using the special characters are going to print the value of the variables.
+# $ special character used to print the value of the variable
+# echo $a
+# $a = ${a}
 # Variables are declared in the way
 a=10
 b=20
