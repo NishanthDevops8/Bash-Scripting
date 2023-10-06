68f8c74bf84d669d1ef57728c329c580814a53d0
NishanthDevops8 <nishanthdevops8@gmail.com>
2023-10-06 19:27:21 +0530

    auto push

--- 06-SpecialVariables.sh
+++ 06-SpecialVariables.sh
@@ -4,4 +4,10 @@
 
 #These are few special variable $0 , $1 --- $9
 
+#$1 is the first argument till $9 we can give the 9 arguments and for 10th argument it will be override by the
+#$1 argument
+# and this arguments can be given by while running the script
+
 echo "this is the name of the script $0"
+echo "Father of the nation $1 and his birthday is on october 2nd"
+
