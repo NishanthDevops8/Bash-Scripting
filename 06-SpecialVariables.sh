0bb88f7124eee0a4b1a90621274aeaf51e78daee
NishanthDevops8 <nishanthdevops8@gmail.com>
2023-10-06 19:24:53 +0530

    auto push

---
+++ 06-SpecialVariables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Here we will discuss about the special variables.
+#special variables give special properties to your variable.
+
+#These are few special variable $0 , $1 --- $9
+
+echo "this is the name of the script $0"
