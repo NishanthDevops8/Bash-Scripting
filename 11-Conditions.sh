e8ec9c43e298d7c0379f13854ba2fcfb59c9f058
NishanthDevops8 <nishanthdevops8@gmail.com>
2023-10-09 20:23:18 +0530

    auto push

---
+++ 11-Conditions.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Here we will discuss about the how conditions works
+
+#use case one if valid conditon is hardcoded.
+
+case $temp in
+    a)
+    echo "Here we are checking the option a"
+    ;;
+    b)
+    echo "Here we are checking the option b"
+    ;;
+    c)
+    echo " Here we are checking the option c"
+    ;;
+    
