fd9eb8cfa2a3a6c6dcc24fc4973e37ea5ab0d8c8
NishanthDevops8 <nishanthdevops8@gmail.com>
2023-10-09 20:46:18 +0530

    auto push

--- 12-Comments.sh
+++ 12-Comments.sh
@@ -2,8 +2,8 @@
 
 #here we will discuss how we can comment in the script.
 
-apple = 100
-boy = 1000
-echo " we have fruits in our shop"
-echo " we mostly have $apple"
+# apple = 100
+# boy = 1000
+# echo " we have fruits in our shop"
+# echo " we mostly have $apple"
 echo " and they are eaten by my $boy"
