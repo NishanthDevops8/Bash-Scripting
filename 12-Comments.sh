b1e3f0f1211286aae3d7d29815be7624be42576d
NishanthDevops8 <nishanthdevops8@gmail.com>
2023-10-10 12:26:52 +0530

    auto push

--- 12-Comments.sh
+++ 12-Comments.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 #here we will discuss how we can comment in the script.
-
-# apple = 100
-# boy = 1000
-# echo " we have fruits in our shop"
-# echo " we mostly have $apple"
+# We can comment  the selected line by using cltr+backslash
+#or we can use <<comment in the starting and COMMENT at the end
+<<COMMENT
+apple = 100
+boy = 1000
+echo " we have fruits in our shop"
+echo " we mostly have $apple"
+COMMENT
 echo " and they are eaten by my $boy"
