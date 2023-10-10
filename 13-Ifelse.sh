f3ffca35ca3bed480475d12bb9a0ab864b4ee221
NishanthDevops8 <nishanthdevops8@gmail.com>
2023-10-10 12:46:18 +0530

    auto push

--- 13-Ifelse.sh
+++ 13-Ifelse.sh
@@ -3,12 +3,12 @@
 Action=$1
 
 if [ "Action" == "start" ]; then
-    echo "here we will start the first if "
+    echo "here we will start the first if"
     exit 0
 elif [ "Action" == "stop" ]; then
     echo "here we will enter the first else if"
     exit 1
-else [ "Action" == "go" ]; then
+else [ "Action" == "go" ];
     echo "thos is the third else condition"
     exit 2
 fi
