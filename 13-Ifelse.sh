2987925b6cb529f1dc9af540231301ade035b861
NishanthDevops8 <nishanthdevops8@gmail.com>
2023-10-10 12:42:11 +0530

    auto push

---
+++ 13-Ifelse.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#here we will discuss the ifelse conditions
+Action=$1
+
+if ["Action" == "start"] ; then
+echo "here we will start the first if "
+exit 0
+elif ["Action" == "stop"] ; then
+echo "here we will enter the first else if"
+exit 1
+else ["Action" == "go"] ; then
+echo "thos is the third else condition"
+exit 2
+fi
