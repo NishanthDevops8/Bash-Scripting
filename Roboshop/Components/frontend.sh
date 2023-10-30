148a42d5d4762a9b95704812c08654d19527f445
NishanthDevops8 <nishanthdevops8@gmail.com>
2023-10-30 16:49:54 +0530

    auto push

--- Roboshop/Components/frontend.sh
+++ Roboshop/Components/frontend.sh
@@ -22,3 +22,23 @@ echo -n "starting nginx"
 systemctl enable nginx &>> /tmp/frontend.log
 systemctl start nginx  &>> /tmp/frontend.log
 status $?
+echo -n "downloading the zip"
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+status $?
+echo -n " cleanup the frontend"
+cd /usr/share/nginx/html
+rm -rf * &>> /tmp/frontend.log
+status $?
+echo -n "extracting frontend"
+unzip /tmp/frontend.zip &>> /tmp/frontend.log
+status $?
+echo -n" sorting the frontend files"
+mv frontend-main/*
+mv static/* 
+rm -rf frontend-main README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+status $?
+echo -n "starting frontend"
+systemctl daemon-reload &>> /tmp/frontend.log
+systemctl restart nginx  &>> /tmp/frontend.log
+status $?
