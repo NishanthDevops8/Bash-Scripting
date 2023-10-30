ca9363b3bacaa5fe3ab21d9b3ff95c43aa86308c
NishanthDevops8 <nishanthdevops8@gmail.com>
2023-10-30 17:05:27 +0530

    auto push

--- Roboshop/Components/frontend.sh
+++ Roboshop/Components/frontend.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+logifle = "/tmp/frontend.log"
+component ="frontend"
 status(){
 
     if [ $1 -eq 0 ]; then
@@ -15,22 +16,22 @@ exit 1
 fi
 echo -e "\e[32mconfiguring frontend\e[0m"
 echo -n "Installing the frontend"
-yum install nginx -y &>> /tmp/frontend.log
+yum install nginx -y &>> ${logfile}
 status $?
 
 echo -n "starting nginx"
-systemctl enable nginx &>> /tmp/frontend.log
-systemctl start nginx  &>> /tmp/frontend.log
+systemctl enable nginx &>> ${logfile}
+systemctl start nginx  &>> ${logfile}
 status $?
 echo -n "downloading the zip"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 status $?
 echo -n " cleanup the frontend"
 cd /usr/share/nginx/html
-rm -rf * &>> /tmp/frontend.log
+rm -rf * &>> ${logfile}
 status $?
 echo -n "extracting frontend"
-unzip /tmp/frontend.zip &>> /tmp/frontend.log
+unzip /tmp/frontend.zip &>> ${logfile}
 status $?
 echo -n" sorting the frontend files"
 mv frontend-main/* .
@@ -39,6 +40,6 @@ rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 status $?
 echo -n "starting frontend"
-systemctl daemon-reload &>> /tmp/frontend.log
-systemctl restart nginx  &>> /tmp/frontend.log
+systemctl daemon-reload &>> ${logfile}
+systemctl restart nginx  &>> ${logfile}
 status $?
