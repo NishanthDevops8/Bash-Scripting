b121f81b08dfb7a0a1aa437dd78366600f530133
NishanthDevops8 <nishanthdevops8@gmail.com>
2023-10-30 16:11:09 +0530

    auto push

--- Roboshop/Components/frontend.sh
+++ Roboshop/Components/frontend.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+status(){
+
+    if [ $1 -eq 0 ]; then
+    echo -e "\e[32msuccess\e[0m"
+else 
+   echo  -e "\e[31mfailed\e[0m" 
+fi
+}
 User_id=$(id -u)
 if [ $User_id -ne 0 ] ; then
 echo -e "\e[31m Script is expected to execute by the root user\e[0m" 
@@ -9,17 +17,9 @@ echo -e "\e[32mconfiguring frontend\e[0m"
 echo -n "Installing the frontend"
 yum install nginx -y &>> /tmp/frontend.log
 
-if [ $? -eq 0 ]; then
-    echo -e "\e[32msuccess\e[0m"
-else 
-   echo  -e "\e[31mfailed\e[0m" 
-fi
+
 echo -n "starting nginx"
 
 systemctl enable nginx &>> /tmp/frontend.log
 systemctl start nginx  &>> /tmp/frontend.log
-if [ $? -eq 0 ]; then
-    echo -e "\e[32msuccess\e[0m"
-else 
-   echo  -e "\e[31mfailed\e[0m" 
-fi
+$?
