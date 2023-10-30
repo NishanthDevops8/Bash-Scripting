76600a8ebee27b05e9934cd8fd4e9b0a60b0b098
NishanthDevops8 <nishanthdevops8@gmail.com>
2023-10-30 12:25:29 +0530

    auto push

--- Roboshop/Components/frontend.sh
+++ Roboshop/Components/frontend.sh
@@ -1,17 +1,2 @@
 #!/bin/bash
-echo "This is the frontend"
-USER_ID=$(id -u)
-
-if [ $USER_ID -ne 0 ] ; then
- echo -e "\e[32mScript need to execute by the sudo user\e[0m"
- exit 1
-fi
-yum install nginx -y &>>../tmp/frontend.config
-
-if [ $? -eq 0 ] ; then
-   echo -e "\e[32m Success \e[0m"
-else
-    echo -e "\e[30m Failed \e[0m"
-
-fi
-
+echo " this is the frontend"
