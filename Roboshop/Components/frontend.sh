b8fa62fe4ef12a24ec6cf8728d202a6193431f8f
NishanthDevops8 <nishanthdevops8@gmail.com>
2023-10-30 12:45:11 +0530

    auto push

--- Roboshop/Components/frontend.sh
+++ Roboshop/Components/frontend.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-echo " configuring frontend"
-yum install nginx -y
+
 User_id=$(id -u)
 if [ $User_id -ne 0 ] ; then
 echo -e "\e[32m Script is expected to execute by the root user\e[0m" 
 exit 1
 fi
+echo " configuring frontend"
+yum install nginx -y
