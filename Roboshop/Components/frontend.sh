17e6929fb4be6d9b0a735ca6d425c26bde3dc2b2
NishanthDevops8 <nishanthdevops8@gmail.com>
2023-10-30 15:48:43 +0530

    auto push

--- Roboshop/Components/frontend.sh
+++ Roboshop/Components/frontend.sh
@@ -5,12 +5,12 @@ if [ $User_id -ne 0 ] ; then
 echo -e "\e[31m Script is expected to execute by the root user\e[0m" 
 exit 1
 fi
-
-yum install abc -y &>> /tmp/frontend.log
 echo -e "\e[32mconfiguring frontend\e[0m"
 echo -n "Installing the frontend"
+yum install abc -y &>> /tmp/frontend.log
 
-if [ $? -eq 0 ] ; then
+
+if [ $? -eq 0 ]; then
     echo -e "\e[32msuccess\e[0m"
 else 
    echo  -e "\e[31mfailed\e[0m" 
